d796b4abe5f1b7f6493a22f972b8f26c6db2d2f2
Michael Dunlap <dunlapm@uw.edu>
2008-06-25 22:44:28 +0000

    widget properties don't exist until one is instanciated.
    svn path=/Products.AddRemoveWidget/branches/dunlapm.keywordwork/; revision=9801

--- Products/AddRemoveWidget/AddRemoveWidget.py
+++ Products/AddRemoveWidget/AddRemoveWidget.py
@@ -64,9 +64,9 @@ class AddRemoveWidget(TypesWidget):
             return empty_marker
         return value, {}
 
-    def is_keyword_field(self, field, source = self.vocab_source):
+    def is_keyword_field(self, field, source):
         """Returns whether or not a given field has a corresponding KeywordIndex
-        in the specified catalog (defaults to the widget's vocab source).
+        in the specified catalog (source).
         """
         catalog = getToolByName(self,source)
         idxs = catalog.index_objects()
