62bf7ee312c061a15ef6dd9088bb8242bade8532
Maurits van Rees <maurits@vanrees.org>
2009-11-04 00:20:41 +0000

    rst fixes, add the history to the long description.
    svn path=/Products.AddRemoveWidget/trunk/; revision=11596

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ import os
 # attention, for BBB-reasons, the version is also held in the packages
 # version.txt file. the file can be removed if we give up support of Plone<3.3.
 version = open(os.path.join("Products", "AddRemoveWidget", "version.txt")).read().strip()
+readme = open(os.path.join("Products", "AddRemoveWidget", "README.txt")).read()
+history = open(os.path.join("docs", "HISTORY.txt")).read()
+long_description = readme + "\n\n" + history
 
 setup(name='Products.AddRemoveWidget',
       version=version,
       description="AddRemoveWidget + ComboBoxWidget for Plone",
-      long_description=open(os.path.join("Products", "AddRemoveWidget", "README.txt")).read(),
+      long_description=long_description,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
