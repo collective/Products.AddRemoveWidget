64e8e878506bc05fd23c060b72967c2b92264d4b
Maurits van Rees <maurits@vanrees.org>
2012-04-29 01:46:40 +0200

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-import os
 
 version = '1.5.0.dev0'
 readme = open("README.rst").read()
@@ -10,11 +9,16 @@ setup(name='Products.AddRemoveWidget',
       version=version,
       description="AddRemoveWidget + ComboBoxWidget for Plone",
       long_description=long_description,
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Framework :: Plone",
+        "Framework :: Plone :: 3.3",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
         "Framework :: Zope2",
         ],
       keywords='Plone Archetypes Products AddRemoveWidget ComboBoxWidget',
